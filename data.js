bb8bfe7a28e06ec5bf36046ac045f6f0788e5a72
fiaga19 <stephfiaga19@gmail.com>
2026-03-01 06:25:41 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,14 @@
+{
+  "diplomes": [
+    {
+      "nom": "JEAN DUPONT",
+      "cle": "4A8B-2C9D-1E0F",
+      "intitule": "Baccalauréat Général",
+      "naissance": "15/04/2002",
+      "academie": "Académie de Paris",
+      "session": "Juin 2020",
+      "specialite": "S - Scientifique (Ancien régime)",
+      "mention": "Bien"
+    }
+  ]
+}
